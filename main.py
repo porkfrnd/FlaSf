e1d2ed71a521cdf7019fe4e1b3f6f9c3b8c1ffe7
HEkcingEz <100692100+HEkcingEz@users.noreply.github.com>
2022-03-07 21:12:46 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import colorama
-from colorama import Fore, Style
+from colorama import Fore
 
 colorama.init()
 
+os.system("cls")
 os.system('title [Beta] FlaSLF || Made by CatNomy')
 
 print(f"""
@@ -15,10 +16,12 @@ print(f"""
 ██║     ███████╗██║  ██║███████║██║      |   |  ╚██████╔╝███████╗   ██║       ███████╗███████╗███████╗███████╗
 ╚═╝     ╚══════╝╚═╝  ╚═╝╚══════╝╚═╝      |   |   ╚═════╝ ╚══════╝   ╚═╝       ╚══════╝╚══════╝╚══════╝╚══════╝
 {Fore.RED}
-1. Discord Bot
-2. Bot Nuker
-3. Token gen        
-                                                                              
+1. Discord Bot{Fore.BLUE}
+2. Bot Nuker{Fore.RED}
+3. Token gen {Fore.BLUE}       
+4. SelfBot  {Fore.RED}
+5. Ip lookup {Fore.BLUE}
+6. Pinger {Fore.RED}                                                           
 """)
 print(Fore.RESET)
 choice = input(f'{Fore.BLUE}Spit ur option out [>')
@@ -30,5 +33,15 @@ if choice in ("1", "Discord Bot", "discord bot"):
     os.system('py ./bot/main.py')
 if choice in ("2", "Bot Nuker", "bot nuker"):
     os.system('py ')
-
+if choice in ("5", "ip lookup", "Ip Lookup"):
+    os.system('py ./iplookup/ipmap.py')
+if choice in ("6", "ip lookup", "Ip Lookup"):
+    pinger_ip = input("The ip >")
+    pinger_port = input("The port >")
+    os.system("cd pinger")
+    os.system("powershell")
+    os.system(f"./main.py {pinger_ip} {pinger_port}")
+    os.system(f"nslookup {pinger_ip}")
+    os.system(f"ping {pinger_ip}")
+    os.system("cd ../")
 os.system('pause')
